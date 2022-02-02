df2666368588c5098f2de22392e92fd19fa211dd
Andru Cherny <wiroatom@gmail.com>
2022-02-02 17:35:06 +0200

    fix

--- .pipelines/build_images.sh
+++ .pipelines/build_images.sh
@@ -4,9 +4,17 @@ printenv | grep GITHUB_REF
 echo "---------------------------------------"
 sleep 2
 set -exu
-DOCKER_IMAGE_VERSION=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
 
-if [ "${DOCKER_IMAGE_VERSION}" == "master" ]; then
-  DOCKER_IMAGE_VERSION="latest"
+DOCKER_PUSH_TO_REGISTRY=false
+if [ ${GITHUB_REF_TYPE} == "branch" ]; then
+
+  if [ ${GITHUB_REF_NAME} == "master" ] || [ ${GITHUB_REF_NAME} == "main" ]; then
+    DOCKER_IMAGE_VERSION=latest
+    DOCKER_PUSH_TO_REGISTRY=true
+  else
+    DOCKER_IMAGE_VERSION=${GITHUB_REF_NAME}
+  fi;
+elif [ ${GITHUB_REF_TYPE} == "tag" ]; then
+    DOCKER_IMAGE_VERSION=${GITHUB_REF_NAME}
+    DOCKER_PUSH_TO_REGISTRY=true
 fi;
-
