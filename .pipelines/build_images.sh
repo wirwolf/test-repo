ca5b8ae914ce19430a516d3dcc2b0ce91d278694
Andru Cherny <wiroatom@gmail.com>
2022-02-02 17:26:14 +0200

    fix

--- .pipelines/build_images.sh
+++ .pipelines/build_images.sh
@@ -10,15 +10,3 @@ if [ "${DOCKER_IMAGE_VERSION}" == "master" ]; then
   DOCKER_IMAGE_VERSION="latest"
 fi;
 
-
-if [ ${GITHUB_REF_TYPE} == "branch" ]; then
-
-  if [ ${GITHUB_REF_NAME} == "master" ] || [ ${GITHUB_REF_NAME} == "main" ]; then
-    DOCKER_IMAGE_VERSION=latest
-  else
-    DOCKER_IMAGE_VERSION=${GITHUB_REF_NAME}
-  fi;
-elif [ ${GITHUB_REF_TYPE} == "tag" ]; then
-    DOCKER_IMAGE_VERSION=${GITHUB_REF_NAME}
-fi;
-
